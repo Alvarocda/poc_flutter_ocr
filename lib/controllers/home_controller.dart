068662d11a03b92fed8416a69c1fc2a2ef0670f4
Álvaro Claro dos Anjos <alvarocda@gmail.com>
2022-08-05 13:59:25 -0400

    Improved plate detection

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -27,7 +27,7 @@ class HomeController {
   final ValueNotifier<bool> isStreaming = ValueNotifier<bool>(false);
   bool _isProcessing = false;
   TextRecognizer? _textRecognizer = TextRecognizer();
-  final RegExp regexPlaca = RegExp('[a-zA-Z]{3}[0-9][A-Za-z0-9][0-9]{2}', caseSensitive: false);
+  final RegExp plateRegex = RegExp('[a-zA-Z]{3}[0-9][A-Za-z0-9][0-9]{2}', caseSensitive: false);
 
   ///
   ///
@@ -64,7 +64,7 @@ class HomeController {
           if (!_isProcessing) {
             InputImage? inputImage = _getStreamInputImage(image);
             if (inputImage != null) {
-              platesDetected.value = await _detectText(inputImage) ?? '';
+              platesDetected.value = await _processImage(inputImage) ?? '';
             }
           }
         },
@@ -124,76 +124,114 @@ class HomeController {
     XFile? xFile = await imagePicker.pickImage(source: ImageSource.gallery);
     if (xFile != null) {
       InputImage inputImage = InputImage.fromFilePath(xFile.path);
-      platesDetected.value = await _detectText(inputImage) ?? '';
+      platesDetected.value = await _processImage(inputImage) ?? '';
     }
   }
 
   ///
   ///
   ///
-  Future<String?> _detectText(InputImage inputImage) async {
+  Future<String?> _processImage(InputImage inputImage) async {
     _isProcessing = true;
     _textRecognizer ??= TextRecognizer();
     RecognizedText recognizedText = await _textRecognizer!.processImage(inputImage);
-
     textDetected.value = recognizedText.text;
-    StringBuffer plates = StringBuffer();
-    List<CustomPaint> customPaints = <CustomPaint>[];
-    // print('TEXTO RECONHECIDO: ${recognizedText.text}');
-    if (regexPlaca.hasMatch(
-      recognizedText.text.trim().replaceAll(' ', '').replaceAll('-', '').replaceAll('.', ''),
-    )) {
+    StringBuffer? plates;
+    List<CustomPaint>? customPaints;
+
+    ///If somewhere in all the detected text it finds a plate, the algorithm will display the plates found on the screen.
+    if (plateRegex.hasMatch(_normalizePlate(recognizedText.text))) {
       int elementIndex = 0;
       for (TextBlock block in recognizedText.blocks) {
         for (TextLine line in block.lines) {
           elementIndex = 0;
           for (TextElement element in line.elements) {
-            print('Text Element: ${element.text}');
-            String detectedPlate = element.text.trim().replaceAll(' ', '').replaceAll('-', '').replaceAll('.', '');
+            String detectedPlate = _normalizePlate(element.text);
+
+            /// If the text is not exactly 7 characters long, it will try to concatenate the text of the previous
+            /// element with the current element and check if the junction of the two forms a valid plate.
+            /// As the algorithm is working with TextElements, it ends up splitting into different elements when it
+            /// detects a space in the middle of the plate.
             if (detectedPlate.length == 7) {
-              _highlightDetectedPlate(detectedPlate, plates, element.boundingBox, inputImage, customPaints);
-            } else if (RegExp(r'^[0-9]{4}$').hasMatch(detectedPlate)) {
+              if (_isValidPlate(detectedPlate)) {
+                plates ?? StringBuffer();
+                customPaints ??= <CustomPaint>[];
+                _highlightDetectedPlate(detectedPlate, plates!, element.boundingBox, inputImage, customPaints);
+              }
+
+              /// If the text in the current element has less than 7 letters, it will check if the text size is 4 characters
+              /// and if it is composed of 4 numbers, if it is composed of 4 numbers, it will check if the text in the
+              /// previous element is composed of 3 letters, if both conditions are true, the algorithm will understand
+              /// that it can be a plate and will assemble a String with the junction of the two.
+            } else if (detectedPlate.length == 4 && RegExp(r'^[0-9]{4}$').hasMatch(detectedPlate)) {
               TextElement lastTextElement = line.elements[elementIndex - 1];
-              String detectedPlate = '${lastTextElement.text}${element.text}';
-              detectedPlate = detectedPlate.trim().replaceAll(' ', '').replaceAll('-', '').replaceAll('.', '');
-              if (detectedPlate.length == 7) {
-                Rect rect = Rect.fromLTRB(
-                  lastTextElement.boundingBox.left,
-                  element.boundingBox.top,
-                  element.boundingBox.right,
-                  element.boundingBox.bottom,
-                );
-                _highlightDetectedPlate(detectedPlate, plates, rect, inputImage, customPaints);
+              if (RegExp('[a-zA-Z]{3}').hasMatch(lastTextElement.text)) {
+                String detectedPlate = '${lastTextElement.text}${element.text}';
+                detectedPlate = _normalizePlate(detectedPlate);
+
+                if (detectedPlate.length == 7) {
+                  if (_isValidPlate(detectedPlate)) {
+                    Rect rect = Rect.fromLTRB(
+                      lastTextElement.boundingBox.left,
+                      element.boundingBox.top,
+                      element.boundingBox.right,
+                      element.boundingBox.bottom,
+                    );
+                    plates ?? StringBuffer();
+                    customPaints ??= <CustomPaint>[];
+                    _highlightDetectedPlate(detectedPlate, plates!, rect, inputImage, customPaints);
+                  }
+                }
               }
             }
             elementIndex++;
           }
         }
       }
-      if (plates.isNotEmpty) {
+      if (plates != null && plates.isNotEmpty) {
         _isProcessing = false;
         platesWidget.add(customPaints);
         return plates.toString();
       }
     }
     platesWidget.add(null);
-    plates.clear();
+    plates?.clear();
     _isProcessing = false;
     return null;
   }
 
+  /// Prepares the text to be
+  ///
+  ///
+  String _normalizePlate(String plate) {
+    return plate.trim().replaceAll(' ', '').replaceAll('-', '').replaceAll('.', '');
+  }
+
+  ///
+  ///
+  ///
+  bool _isValidPlate(String plate) {
+    return plateRegex.hasMatch(plate);
+  }
+
+  ///
+  ///
+  ///
   void _highlightDetectedPlate(
-      String detectedPlate, StringBuffer plates, Rect rect, InputImage inputImage, List<CustomPaint> customPaints) {
-    if (regexPlaca.hasMatch(detectedPlate)) {
-      plates.writeln(detectedPlate);
-      CustomPaint customPaint = CustomPaint(
-        painter: CustomTextRecognizerPainter(
-          rect,
-          inputImage.inputImageData!.size,
-          inputImage.inputImageData!.imageRotation,
-        ),
-      );
-      customPaints.add(customPaint);
-    }
+    String detectedPlate,
+    StringBuffer plates,
+    Rect rect,
+    InputImage inputImage,
+    List<CustomPaint> customPaints,
+  ) {
+    plates.writeln(detectedPlate);
+    CustomPaint customPaint = CustomPaint(
+      painter: CustomTextRecognizerPainter(
+        rect,
+        inputImage.inputImageData!.size,
+        inputImage.inputImageData!.imageRotation,
+      ),
+    );
+    customPaints.add(customPaint);
   }
 }
